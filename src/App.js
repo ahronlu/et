1d10d7acb535aefecd9cc3ed4f561361bbb7d24b
koftov <ahronlu@gmail.com>
2020-12-20 19:51:33 +0200

    details component and styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { Grid } from "@material-ui/core";
 import React from "react";
+import Details from "./components/Details/Details";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Grid
+        container
+        spacing={0}
+        alignItems="center"
+        justify="center"
+        style={{ height: "100vh" }}
+      >
+        <Grid item xs={12} sm={4}>
+          <Details />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          {/* <Details /> */}
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Details />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default App;
